1ae9e184d978d7219e93e284394b58b987336f00
johnt1989 <75329354+johnt1989@users.noreply.github.com>
2022-10-14 03:42:52 -0700

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Bootloader
 BOARD_VENDOR := samsung
 TARGET_SOC := msmnile
@@ -39,12 +55,15 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno640
 QCOM_BOARD_PLATFORMS += msmnile
 
 # Kernel
+MKBOOTIMG := device/samsung/d2q/mkbootimg
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/d2q/bootimg.mk
 TARGET_PREBUILT_KERNEL := device/samsung/d2q/prebuilt/Image.gz-dtb
 BOARD_PREBUILT_DTBOIMAGE := device/samsung/d2q/prebuilt/recoverydtbo
 BOARD_INCLUDE_RECOVERY_DTBO := true
 TARGET_KERNEL_ARCH := arm64
 
 # Boot
+
 BOARD_BOOT_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image androidboot.selinux=permissive
@@ -59,8 +78,6 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset 
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION) --pagesize $(BOARD_KERNEL_PAGESIZE) --board "SRPSC04A005"
 BOARD_MKBOOTIMG_ARGS += --recovery_dtbo $(BOARD_PREBUILT_DTBOIMAGE)
-MKBOOTIMG := device/samsung/d2q/mkbootimg
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/d2q/bootimg.mk
 
 # Recovery
 BOARD_HAS_LARGE_FILESYSTEM := true
