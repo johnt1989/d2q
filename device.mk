6325414ca488d60b2676e198aaa95e71548d71be
johnt1989 <johnjohnt1989@gmail.com>
2023-04-26 16:48:48 -0700

    .

---
+++ device.mk
@@ -0,0 +1,4 @@
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
--- twrp_d2q.mk
+++ twrp_d2q.mk
@@ -8,6 +8,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+# Inherit device configuration
+$(call inherit-product, device/samsung/d2q/device.mk)
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
