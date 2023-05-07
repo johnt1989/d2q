20443f14d58baef9b6a8445588f0033d37de21aa
johnt1989 <75329354+johnt1989@users.noreply.github.com>
2023-05-07 07:46:18 -0700

    Add files via upload

--- device.mk
+++ device.mk
@@ -3,4 +3,14 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-TARGET_USE_QTI_BT_STACK := true
+# qcom decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Dependencies
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
